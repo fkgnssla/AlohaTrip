5695d084715cd0afca91e99a541fef4a04aa759e
Yuri-CHOE <78342803+Yuri-CHOE@users.noreply.github.com>
2024-05-21 15:07:55 +0900

    임시 main 화면

--- AlohaTrip_Vue/src/views/MainView.vue
+++ AlohaTrip_Vue/src/views/MainView.vue
@@ -1,7 +1,20 @@
 <script setup></script>
 
 <template>
-  <div>Main 화면 입니다.</div>
+  <div class="hotPlaceBanner">
+            <div class="position-absolute top-50 start-50 translate-middle">Main View</div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.hotPlaceBanner{
+  background-image: linear-gradient( rgba(0,0,0, 0.55), rgba(0,0,0, 0.55) ),url("@/assets/img/common/MainBannerImg.jpg");
+  background-repeat : no-repeat;
+  background-size : cover;
+  height: 900px;
+  color: #7AEBB8;
+  font-weight: bold;
+  font-size: 35px;
+  position: relative;
+}
+</style>
