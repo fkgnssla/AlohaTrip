e21f1c4c4c80bcd6fa155b3e76bf917f7ec5ea24
김형민 <fkgnssla@naver.com>
2024-05-22 10:34:06 +0900

    fix: attraction.xml 수정

--- AlohaTrip_Vue/src/views/MainView.vue
+++ AlohaTrip_Vue/src/views/MainView.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div>
-    <video width="640" height="360" autoplay muted>
+    <video width="640" height="360" autoplay loop muted>
         <source src="../assets/video/seoul.mp4" type="video/mp4">
         Your browser does not support the video tag.
     </video>
